a21edd4e6604bbe2ba6c4cc904cc88415aba3bea
Julien Maulny <doubjulien@hotmail.fr>
2018-01-22 18:21:25 +0100

    add tests for undo, add tests coverage

--- test/engine/ArbiterTest.js
+++ test/engine/ArbiterTest.js
@@ -28,6 +28,18 @@ describe('Arbiter', () => {
             arbiter.selection.should.be.an.instanceOf(Unit);
             expect(world.getEntityAt(new Hex(-3, 0, 3))).to.be.null;
         });
+
+        it('breaks when no kingdom selected', () => {
+            const worldGenerator = new WorldGenerator('constant-seed-2');
+            const world = worldGenerator.generate(createTestPlayers());
+
+            world.setEntityAt(new Hex(-3, 0, 3), new Unit());
+
+            const arbiter = new Arbiter(world);
+            arbiter.setCurrentPlayer(world.getKingdomAt(new Hex(-4, 1, 3)).player);
+
+            expect(() => {arbiter.takeUnitAt(new Hex(-3, 0, 3))}).to.throw(/kingdom must be selected/);
+        });
     });
 
     describe('placeAt', () => {
@@ -586,6 +598,104 @@ describe('Arbiter', () => {
             }
 
             expect(world.getEntityAt(new Hex(2, -1, -1))).to.be.an.instanceOf(Tree);
+        });
+    });
+
+    describe('undo', () => {
+        it('undo unit buy when selection is empty', () => {
+            const worldGenerator = new WorldGenerator('constant-seed-5');
+            const world = worldGenerator.generate(createTestPlayers());
+
+            const arbiter = new Arbiter(world);
+            const kingdom = world.getKingdomAt(new Hex(2, -1, -1));
+            arbiter.setCurrentPlayer(kingdom.player);
+            arbiter.setCurrentKingdom(kingdom);
+
+            arbiter.selection = null;
+            kingdom.money = 15;
+
+            arbiter.buyUnit();
+            arbiter.undo();
+
+            expect(arbiter.selection).to.be.null;
+            kingdom.money.should.be.equal(15);
+        });
+
+        it('undo unit buy/upgrade', () => {
+            const worldGenerator = new WorldGenerator('constant-seed-5');
+            const world = worldGenerator.generate(createTestPlayers());
+
+            const arbiter = new Arbiter(world);
+            const kingdom = world.getKingdomAt(new Hex(2, -1, -1));
+            arbiter.setCurrentPlayer(kingdom.player);
+            arbiter.setCurrentKingdom(kingdom);
+
+            arbiter.selection = new Unit();
+            arbiter.selection.level = 1;
+            kingdom.money = 15;
+
+            arbiter.buyUnit();
+            arbiter.undo();
+
+            expect(arbiter.selection).to.be.an.instanceOf(Unit);
+            arbiter.selection.level.should.be.equal(1);
+            kingdom.money.should.equal(15);
+        });
+
+        it('undo takeUnit', () => {
+            const worldGenerator = new WorldGenerator('constant-seed-5');
+            const world = worldGenerator.generate(createTestPlayers());
+
+            const arbiter = new Arbiter(world);
+            const kingdom = world.getKingdomAt(new Hex(2, -1, -1));
+            arbiter.setCurrentPlayer(kingdom.player);
+            arbiter.setCurrentKingdom(kingdom);
+            world.setEntityAt(new Hex(2, -1, -1), new Unit());
+
+            arbiter.takeUnitAt(new Hex(2, -1, -1));
+            arbiter.undo();
+
+            expect(arbiter.selection).to.be.null;
+            expect(world.getEntityAt(new Hex(2, -1, -1))).to.be.an.instanceOf(Unit);
+        });
+
+        it('undo placeAt when placed in own kingdom', () => {
+            const worldGenerator = new WorldGenerator('constant-seed-5');
+            const world = worldGenerator.generate(createTestPlayers());
+
+            const arbiter = new Arbiter(world);
+            const kingdom = world.getKingdomAt(new Hex(2, -1, -1));
+            arbiter.setCurrentPlayer(kingdom.player);
+            arbiter.setCurrentKingdom(kingdom);
+            arbiter.selection = new Unit();
+
+            arbiter.placeAt(new Hex(2, -1, -1));
+            arbiter.undo();
+
+            expect(arbiter.selection).to.be.an.instanceOf(Unit);
+            expect(world.getEntityAt(new Hex(2, -1, -1))).to.be.null;
+        });
+    });
+
+    describe('undoAll', () => {
+        it('undo double unit buy when selection is empty', () => {
+            const worldGenerator = new WorldGenerator('constant-seed-5');
+            const world = worldGenerator.generate(createTestPlayers());
+
+            const arbiter = new Arbiter(world);
+            const kingdom = world.getKingdomAt(new Hex(2, -1, -1));
+            arbiter.setCurrentPlayer(kingdom.player);
+            arbiter.setCurrentKingdom(kingdom);
+
+            arbiter.selection = null;
+            kingdom.money = 42;
+
+            arbiter.buyUnit();
+            arbiter.buyUnit();
+            arbiter.undoAll();
+
+            expect(arbiter.selection).to.be.null;
+            kingdom.money.should.be.equal(42);
         });
     });
 
