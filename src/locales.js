e24e24dd0484700d78570f2bab2edf41767642e3
Julien Maulny <julien.maulny@protonmail.com>
2018-03-12 14:12:55 +0100

    detect language

--- src/locales.js
+++ src/locales.js
@@ -1,12 +1,19 @@
 import i18next from 'i18next';
+import LngDetector from 'i18next-browser-languagedetector';
 import { en, fr, es } from './engine/locales';
 
-i18next.init({
-  //debug: true,
-  lng: 'en',
-  fallbackLng: 'en',
-  defaultNs: 'translation',
-});
+i18next
+    .use(LngDetector)
+    .init({
+        debug: true,
+        fallbackLng: 'en',
+        defaultNs: 'translation',
+        detection: {
+            order: ['querystring', 'navigator'],
+            lookupQuerystring: 'lng'
+        }
+    })
+;
 
 i18next.addResourceBundle('en', 'translation', en);
 i18next.addResourceBundle('fr', 'translation', fr);
