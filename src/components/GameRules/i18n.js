adce7d3c0431a34ad9cd6d27062271a4515812fe
Julien Maulny <julien.maulny@protonmail.com>
2019-12-11 19:17:54 +0100

    Remove unused spanish locale

--- src/components/GameRules/i18n.js
+++ src/components/GameRules/i18n.js
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 import LngDetector from 'i18next-browser-languagedetector';
 import i18nextOptions from '../../i18n/i18nextOptions';
-import { en, fr, es } from './locales';
+import { en, fr } from './locales';
 
 const i18nGameRules = i18next.createInstance();
 
@@ -17,6 +17,5 @@ i18nGameRules
 
 i18nGameRules.addResourceBundle('en', 'translation', en);
 i18nGameRules.addResourceBundle('fr', 'translation', fr);
-i18nGameRules.addResourceBundle('es', 'translation', es);
 
 export default i18nGameRules;
