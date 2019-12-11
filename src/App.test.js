241843f292d9447d8b33ad7f3dd6552c65e270ec
Julien Maulny <julien.maulny@protonmail.com>
2019-12-11 19:20:26 +0100

    Add react functionnal tests to reveal bugs just on starting app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Arbiter, WorldConfig, WorldGenerator } from './engine';
+import { OpenHexGrid } from './components';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders game without crashing', () => {
+  const worldConfig = WorldConfig({
+    size: 6,
+  });
+  const world = WorldGenerator.generate('seed-test', worldConfig);
+  const arbiter = new Arbiter(world);
+
+  const div = document.createElement('div');
+  ReactDOM.render(<OpenHexGrid
+    world={ world }
+    arbiter={ arbiter }
+  />, div);
 });
